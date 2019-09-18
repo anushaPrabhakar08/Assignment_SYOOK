b968cfec27b318c52cf00d958e6898fa71610aa0
anushaPrabhakar08 <anushaanudec8@gmail.com>
2019-09-18 19:30:46 +0530

    edit delete and save functionality

--- src/index.js
+++ src/index.js
@@ -14,3 +14,32 @@ function searchFunction() {
         }
     }
 }
+
+function edit_row(no) {
+    document.getElementById("edit_button" + no).style.display = "none";
+    document.getElementById("save_button" + no).style.display = "block";
+
+    var title = document.getElementById("titlerow" + no);
+    var description = document.getElementById("descriptionrow" + no);
+
+    var title_data = title.innerHTML;
+    var description_data = description.innerHTML;
+
+    title.innerHTML = "<input type='text' id='title_text" + no + "' value='" + title_data + "'>";
+    description.innerHTML = "<input type='text' id='description_text" + no + "' value='" + description_data + "'>";
+}
+
+function save_row(no) {
+    var title_val = document.getElementById("title_text" + no).value;
+    var description_val = document.getElementById("description_text" + no).value;
+
+    document.getElementById("titlerow" + no).innerHTML = title_val;
+    document.getElementById("descriptionrow" + no).innerHTML = description_val;
+
+    document.getElementById("edit_button" + no).style.display = "block";
+    document.getElementById("save_button" + no).style.display = "none";
+}
+
+function delete_row(no) {
+    document.getElementById("row" + no + "").outerHTML = "";
+}
